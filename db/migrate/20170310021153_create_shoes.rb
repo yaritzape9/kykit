ac98e0feef28b8bc26716ea973bc0a31d1155fa8
yaritza perez <yaritzape9@gmail.com>
2017-03-09 18:35:31 -0800

    adds rails skeleton

---
+++ db/migrate/20170310021153_create_shoes.rb
@@ -0,0 +1,18 @@
+class CreateShoes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :shoes do |t|
+      t.string :title, null: false, limit: 64
+      t.string :brand, null: false
+      t.integer :size, null: false
+      t.float :price, null: false
+      t.string :condition, null: false
+      t.integer :zip_code, null: false
+      t.integer :distance, null: false
+      t.text :description
+      t.integer :seller_id
+
+      t.timestamps
+
+    end
+  end
+end

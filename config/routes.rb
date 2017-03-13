01b8882500c11e3a35a1f237db96b07017c451be
yaritza perez <yaritzape9@gmail.com>
2017-03-13 15:44:44 -0700

    adds config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :shoes
+  resources :categories
+
+  resources :users
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#delete', as: 'logout'
+  root 'shoes#index'
 end
